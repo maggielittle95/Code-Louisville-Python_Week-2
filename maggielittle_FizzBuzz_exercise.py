eb28d89e49d8a9a47a767d2218ca3cbf2ffe1cc8
Maggie  Little <maggielittle95@gmail.com>
2023-08-01 13:48:39 -0400

    revised and submitted FizzBuzz exercise

--- maggielittle_FizzBuzz_exercise.py
+++ maggielittle_FizzBuzz_exercise.py
@@ -1,24 +1,15 @@
-# Define Fizz
-def Fizz(num):
-    if num % 3 == 0:
-        print("Fizz")
-
-# Define Buzz
-def Buzz(num):
-    if num % 5 == 0:
-        print("Buzz")
-
 # Define FizzBuzz
 def FizzBuzz(num):
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
-
-# Output of anything else should be the integer
-    else: print(num) 
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
 
 # Define the range of numbers to iterate through
 for num in range(1, 101):
-    # Call the functions Fizz, Buzz, and FizzBuzz based on the conditions
-    Fizz(num)
-    Buzz(num)
+    # Call the Fizz function to print the appropriate word or number
     FizzBuzz(num)
